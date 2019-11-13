a122f2a66fe0819cf3b817f3e19e10892b38cb1a
John Mark Goodson <john.mark.hat@gmail.com>
2019-11-12 19:22:16 -0500

    carousel now scrolls on left on previous and right on next buttons, several janky behaviours and now looping yet.

--- script.js
+++ script.js
@@ -11,3 +11,35 @@ function toggleMenu () {
 }
 
 toggleMenu();
+
+
+const carousel = document.querySelector('.current-image');
+const carouselImages = document.querySelectorAll(".current-image img");
+
+const previousButton = document.querySelector("#previousButton");
+const nextButton = document.querySelector("#nextButton");
+
+let counter = 1;
+const size = 750; 
+// size should be set to below but it's not registering for some reason
+// so had to manually override.
+// carouselImages[0].clientWidth;
+carousel.style.transform = 'translateX(' + (-size * counter) + 'px)'; 
+
+nextButton.addEventListener("click", function () {
+    if (counter = carouselImages.length) {return};
+    carousel.style.transition = "transform 0.4 ease-in-out";
+    counter++;
+    console.log(counter);
+    carousel.style.transform = 'translateX(' + (-size * counter) + 'px)'; 
+});
+
+previousButton.addEventListener("click", function () {
+    if (counter <= 0) {return};
+    carousel.style.transition = "transform 0.4 ease-in-out";
+    counter--;
+    console.log(counter);
+    carousel.style.transform = 'translateX(' + (-size * counter) + 'px)'; 
+});
+
+// transitionEnd - doesn't work
